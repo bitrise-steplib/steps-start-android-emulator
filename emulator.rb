d9e2cb25b5c776cd8f68b863f0e6862ce5345d94
Krisztián Gödrei <godrekrisztian@gmil.com>
2015-12-04 16:57:46 +0100

    force 32 bit emulator

--- emulator.rb
+++ emulator.rb
@@ -57,7 +57,7 @@ end
 
 def start_emulator(avd_name, uuid)
   emulator = File.join(ENV['android_home'], 'tools/emulator')
-  pid = spawn("#{emulator} -avd #{avd_name} -no-boot-anim -no-skin -noaudio -no-window -prop emu.uuid=#{uuid}", [:out, :err] => ['emulator.log', 'w'])
+  pid = spawn("#{emulator} -force-32bit -avd #{avd_name} -no-boot-anim -no-skin -noaudio -no-window -prop emu.uuid=#{uuid}", [:out, :err] => ['emulator.log', 'w'])
   Process.detach(pid)
 end
 
