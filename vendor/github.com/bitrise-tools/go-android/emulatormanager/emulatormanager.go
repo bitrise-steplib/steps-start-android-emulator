65d43c103262b415b1eb92f6d4a49c683442f609
Tamas Papik <trapacska@gmail.com>
2017-05-08 12:27:35 +0200

    godeps-update

--- vendor/github.com/bitrise-tools/go-android/emulatormanager/emulatormanager.go
+++ vendor/github.com/bitrise-tools/go-android/emulatormanager/emulatormanager.go
@@ -13,19 +13,18 @@ import (
 // Model ...
 type Model struct {
 	binPth string
+	env    string
 }
 
 // New ...
 func New(sdk sdk.AndroidSdkInterface) (*Model, error) {
-	binPth := filepath.Join(sdk.GetAndroidHome(), "emulator", "emulator")
-	exist, err := pathutil.IsPathExists(binPth)
-	if err != nil {
-		return nil, err
-	} else if !exist {
-		binPth = filepath.Join(sdk.GetAndroidHome(), "tools", "emulator")
-		if runtime.GOOS == "linux" {
-			binPth = filepath.Join(sdk.GetAndroidHome(), "tools", "emulator64-arm")
-		}
+	binPth := filepath.Join(sdk.GetAndroidHome(), "emulator", "emulator64-arm")
+
+	env := ""
+	if runtime.GOOS == "linux" {
+		env = "LD_LIBRARY_PATH=" + filepath.Join(sdk.GetAndroidHome(), "emulator", "lib64", "qt", "lib")
+	} else if runtime.GOOS == "darwin" {
+		env = "DYLD_LIBRARY_PATH=" + filepath.Join(sdk.GetAndroidHome(), "emulator", "lib64", "qt", "lib")
 	}
 
 	if exist, err := pathutil.IsPathExists(binPth); err != nil {
@@ -36,6 +35,7 @@ func New(sdk sdk.AndroidSdkInterface) (*Model, error) {
 
 	return &Model{
 		binPth: binPth,
+		env:    env,
 	}, nil
 }
 
@@ -51,5 +51,10 @@ func (model Model) StartEmulatorCommand(name, skin string, options ...string) *c
 
 	args = append(args, options...)
 
-	return command.New(args[0], args[1:]...)
+	commandModel := command.New(args[0], args[1:]...)
+	if model.env != "" {
+		commandModel = command.New(args[0], args[1:]...).AppendEnvs(model.env)
+	}
+
+	return commandModel
 }
